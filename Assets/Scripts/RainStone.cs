f900b662c446d48d41dbbe287e00314c14479fe3
zoniventris <f.zoniventris@gmail.com>
2021-05-01 14:09:38 -0700

    Implement rain stone effect.

--- Assets/Scripts/RainStone.cs
+++ Assets/Scripts/RainStone.cs
@@ -4,6 +4,10 @@ public class RainStone : MonoBehaviour, IInteractable
 {
     public bool Interact()
     {
+        foreach (Planter.Data data in PlanterManager.PersistentData.planterData)
+        {
+            data.harvestTime = Time.time;
+        }
         Destroy(gameObject);
         return true;
     }
