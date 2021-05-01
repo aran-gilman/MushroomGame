871727b40771d233df7ba71f6d3536c375c1715e
zoniventris <f.zoniventris@gmail.com>
2021-05-01 00:35:04 -0700

    Add basic player movement.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"))
+            .normalized * speed;
+    }
+}
