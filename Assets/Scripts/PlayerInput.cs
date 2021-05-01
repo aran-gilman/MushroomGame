88dd2a72f81d3e03f140290bbf9592527d20aa93
zoniventris <f.zoniventris@gmail.com>
2021-05-01 02:19:46 -0700

    Add placeholder movement between forest and garden.

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,20 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerInput : MonoBehaviour
 {
     public float speed = 5.0f;
     public Collider2D interactionCollider;
 
-    private Rigidbody2D rb;
+    private Rigidbody2D rb;   
 
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     
-    void Update()
+    private void Update()
     {
         rb.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"))
             .normalized * speed;
@@ -32,6 +33,24 @@ public class PlayerInput : MonoBehaviour
                     break;
                 }
             }
+        }
+
+        if (Input.GetButtonUp("Teleport"))
+        {
+            Teleport();
+        }
+    }
+
+    private void Teleport()
+    {
+        string activeSceneName = SceneManager.GetActiveScene().name;
+        if (activeSceneName == "Forest")
+        {
+            SceneManager.LoadScene("Garden");
+        }
+        else if (activeSceneName == "Garden")
+        {
+            SceneManager.LoadScene("Forest");
         }
     }
 }
