2439a4094386dc0eec20a4cdc796a64bf256a975
zoniventris <f.zoniventris@gmail.com>
2021-05-01 00:01:12 -0700

    Add Mushroom class and associated objects.

---
+++ Assets/Scripts/Mushroom.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Mushroom : ScriptableObject
+{
+    public Sprite sprite;
+    public string displayName;
+    public int cost;
+    public int value;
+}
