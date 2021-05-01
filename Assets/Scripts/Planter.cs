63a33d590f3a863c4999d70d7e3cc25d36101e9d
zoniventris <f.zoniventris@gmail.com>
2021-05-01 00:02:40 -0700

    Add Planter objects (aka Hardwood Logs).
    The word 'planter' is used to avoid a name collision with 'log'.

---
+++ Assets/Scripts/Planter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planter : MonoBehaviour
+{
+    [Serializable]
+    public class Data
+    {
+        public Mushroom mushroom;
+        public float harvestTime;
+    }
+    public Data data;
+}
---
+++ Assets/Scripts/PlanterManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanterManager : MonoBehaviour
+{
+    public GameObject planterPrefab;
+
+    public static void AddPlanter()
+    {
+        data.planterData.Add(new Planter.Data());
+    }
+
+    public static void Init()
+    {
+        data = new Data();
+        AddPlanter();
+    }
+
+    private class Data
+    {
+        public List<Planter.Data> planterData = new List<Planter.Data>();
+    }
+    private static Data data;
+    
+    private void Start()
+    {
+        if (data == null)
+        {
+            Init();
+        }
+
+        for (int i = 0; i < data.planterData.Count; i++)
+        {
+            Transform spot = transform.GetChild(i);
+            GameObject planter = Instantiate(planterPrefab, spot);
+            planter.GetComponent<Planter>().data = data.planterData[i];
+        }
+    }
+}
